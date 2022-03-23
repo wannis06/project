75f7bc24e51fb324a46c71ffd93784fd0b17bc7f
modaplusieur <89744624+modaplusieur@users.noreply.github.com>
2022-03-23 22:32:15 +0100

    tep

--- setting.py
+++ setting.py
@@ -2,6 +2,17 @@
 from tkinter import *
 
 #creeation de la fonction principale
-def aff_setting():
+def aff_setting(self):
     set = Tk()
+    set.geometry('400x400')
+
+    #creeation du menu pricipale
+    menubar = Menu(set)
+    set.config(menu = menubar)
+
+    #fichier
+    menufichier = Menu(menubar, tearoff=0)
+    menubar.add_cascade(label="Fichier", menu = menufichier)
+
     set.mainloop()
+
